c0b6b8143f78f3fcc1238e5c9b9706b005c1ddf4
gabys12 <99739807+gabys12@users.noreply.github.com>
2022-03-03 17:39:50 -0500

    dictionary

---
+++ tipos de datos/dictionary1.py
@@ -0,0 +1,4 @@
+diccionario = {'nombre' : 'Carlos', 'edad' : 22, 'cursos': ['Python','Django','JavaScript'] }
+print(diccionario['nombre']) 
+print(diccionario['cursos'])
+print(diccionario['cursos'][0])
