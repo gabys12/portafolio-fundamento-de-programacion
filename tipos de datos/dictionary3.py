ed409f1b2f24e19801f08673adb86c7edfe5d42a
gabys12 <99739807+gabys12@users.noreply.github.com>
2022-03-03 17:41:34 -0500

    dictionary

---
+++ tipos de datos/dictionary3.py
@@ -0,0 +1,4 @@
+import random
+clientes = ["Alex","Bob","Carol","Dave","Flow","Katie","Nate"]
+diccionario_descuentos = {cliente:random.randint(1,100) for cliente in clientes}
+print(diccionario_descuentos)
